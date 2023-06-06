75ca5c0590724aceda5426412253ac359fd529c5
jontcont <52921053+JontCont@users.noreply.github.com>
2023-06-06 23:28:45 +0800

    Initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment } from './store/counter/counterSlice';
 
 function App() {
+  const count = useSelector((state: any) => {
+    console.log(state);
+    return state.counter.value;
+  });
+  const dispatch = useDispatch()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h3>React-Redux</h3>
+      <div>
+        Result : {count}
+      </div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
         >
-          Learn React
-        </a>
-      </header>
+          Increment
+        </button>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   );
 }
